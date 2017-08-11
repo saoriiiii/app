232335de0bd85f7c8a5172dd4bac7be79f7f66e9
saoriiiii <saori.shibuta@gmail.com>
2017-08-11 15:14:12 +0900

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,35 @@
+/*
+* Login page
+* @TODO encript login info
+*/
+
+/** @jsx React.DOM */
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <p>Hello, world!</p>
+    );
+  }
+});
+
+React.render(
+  <App></App>,
+  document.getElementById('app')
+);
+
+
+
+
+// var app = React.createClass({
+// render: function() {
+//    return (
+//      <h1>Hello, world!</h1>
+//    );
+//  }
+// });
+//
+// React.render(
+//   <App></App>,
+//   document.getElementById("app");
+// )
